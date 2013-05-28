65b2a3a42b8dd0e32f51a98563bc4612063de415
Daniel Lima <daniel.lima@tre-ma.jus.br>
2013-05-28 09:36:24 -0300

    refactor do SessaoTest para usar o @Before do JUnit

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -1,41 +1,40 @@
 package br.com.caelum.agiletickets.models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class SessaoTest {
 
+	private Sessao sessao;
+
+	@Before
+	public void setup() {
+		sessao = new Sessao();
+	}
 
 	@Test
 	public void deveVenderIngressoSeNumeroDeVagasMaiorQueReservas() throws Exception {
-		Sessao sessao = new Sessao();
         sessao.setTotalIngressos(2);
-
         Assert.assertTrue(sessao.podeReservar(1));
 	}
 
 	@Test
 	public void naoDeveVenderIngressoSeNumeroDeVagasMenorQueReservas() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(2);
-
 		Assert.assertFalse(sessao.podeReservar(3));
 	}
 
 	@Test
 	public void deveVenderIngressoSeNumeroDeVagasIgualDeReservas() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(3);
-
 		Assert.assertTrue(sessao.podeReservar(3));
 	}
 	
 	
 	@Test
 	public void reservarIngressosDeveDiminuirONumeroDeIngressosDisponiveis() throws Exception {
-		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(5);
-
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
